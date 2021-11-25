9c820a53c1aa3467b3038affccf79b6ccb4e4580
sanghh111 <sanghh111@gmail.com>
2021-11-25 21:52:58 +0700

    add tranning_data.py

--- tranning_data.py
+++ tranning_data.py
@@ -1,8 +1,41 @@
-import os
+import os,cv2
+import numpy as np
+from PIL import Image
+from tkinter import messagebox
 
 def tranning_data():
-    path = os.path.join('./data')
-    for i in os.walk(path):
-        print(i)
+    path = os.path.join(os.getcwd()+'/data/')
+    root,dirs,files = os.walk(path)
+    
+    subdirs = root[1]
+        
+    for subdir in subdirs:
+        print('subdir: ', subdir+'/')
+        subpath = os.path.join(path,subdir+'/')
 
-tranning_data()
+        # for indexTrain,sub in os.walk(subpath):
+        faces = []
+        ids = []
+        pictures = {}
+        for root2, dirs2, files2 in os.walk(subpath):
+            pictures = files2
+        if len(pictures)> 300:
+            for pic in pictures:
+                imgpath = subpath + pic
+                print('imgpath: ', imgpath)
+                img = Image.open(imgpath).convert('L')
+                imgNp = np.array(img,'uint8')
+                id = int(pic.split(subdir)[0])
+                faces.append(imgNp)
+                ids.append(id)
+
+            ids = np.array(ids)
+
+            # Train and save classifier
+            clf = cv2.face.LBPHFaceRecognizer_create()
+            clf.train(faces, ids)
+            clf.write("./classifiers/" + subdir + "_classifier.xml")
+            messagebox.showinfo("Thông báo","Dữ liệu của {} đã được tào thành công".format(subdir))
+        else:
+            messagebox.showinfo("Thông báo","Dữ liệu của {} không đủ 300 tấm hình".format(subdir))
+
